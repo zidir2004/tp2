c650d1a1b1375a4a2885acf8a161f6fa7dbf8b32
zidir2004 <148040865+zidir2004@users.noreply.github.com>
2023-10-16 09:01:14 +0200

    Create SimpleTesting.java

---
+++ SimpleTesting.java
@@ -0,0 +1,7 @@
+public class SimpleTesting {
+public static void main(String[] args) {
+Student student = new Student("John");
+String name = student.getName();
+System.out.println("Student name is : "+name);
+}
+}
